021ca09e68106fc0a8a6f33f4d4423b44efdf176
Sebastian <sebxian@gmx.de>
2018-11-21 18:56:55 +0100

    Add Edit component

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,35 @@ import styled from 'styled-components'
 import List from './List.js'
 import Edit from './Edit.js'
 
+
+/* NOT IN USE 
 const Wrapper = styled.div`
   background-color: lightgreen;
-`
+` */
+
+let notes = [
+  { body: 'This is a placeholder note. \n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacus nunc, auctor vestibulum dignissim nec, consectetur ut tellus. In auctor venenatis luctus.' }
+]
 
 class App extends Component {
+
+  saveNote = body => {
+    notes = [
+      { body: body },
+      ...notes
+    ]
+
+    console.log(notes)
+  }
+
   render () {
     return (
       <Router>
-        <Wrapper>
+        <React.Fragment>
           <Route exact path="/" render={() => <List />} />
           <Route path="/list" render={() => <List />} />
-          <Route path="/create" render={() => <Edit />} />
-        </Wrapper>
+          <Route path="/create" render={() => <Edit onSubmit={this.saveNote} />} />
+        </React.Fragment>
       </Router>
     )
   }
--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-rows: auto 40px;
+`
+
+const Textarea = styled.textarea`
+  height: 100%;
+  width: 100%;
+  font-size: 1em;
+  resize: none;
+`
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  border-top-style: solid;
+  border-top-width: 1px;
+  border-top-color: black;
+`
 
 export default class Edit extends Component {
+
+  state = {
+    inputBody: '',
+    toggle: false
+  }
+
+  /* keyDownHandler = () => {
+    //  event.target.inputBody
+  } */
+
+  submitHandler = (event, onSubmit) => {
+    if (this.state.inputBody !== '') {
+      onSubmit(this.state.inputBody)
+      this.setState({ inputBody: '' })
+    } else {
+      console.log('nothing to save')
+      this.refs.textarea.focus()
+    }
+  }
+
   render () {
+    const { onSubmit } = this.props
+
     return (
-      <div>
-        <h1>Edit Component</h1>
-      </div>
+      <Wrapper className="Edit">
+        <main>
+          <Textarea
+            autoFocus
+            ref="textarea"
+            value={this.state.inputBody}
+            placeholder="Write a note..."
+            onChange={event => this.setState({ inputBody: event.target.value })}
+          />
+        </main>
+        <Footer>
+          <span onClick={() => this.submitHandler(onSubmit)}>[Submit]</span>
+        </Footer>
+      </Wrapper>
     )
   }
 }
