55feb5b8a701bf581aae1b1830e41228aedf16a7
Sebastian <sebxian@gmx.de>
2018-11-22 12:06:36 +0100

    Add storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,8 +3,17 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { text, boolean, number } from '@storybook/addon-knobs'
+
+// VANILLA IMPORTS
 
 import { Button, Welcome } from '@storybook/react/demo';
+
+// MY IMPORTS
+
+import Edit from '../components/Edit'
+
+// VANILLA STORIES
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -17,3 +26,10 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+
+// MY STORIES
+
+storiesOf('Edit', module)
+  .add('Create and Edit notes', () => <Edit onSubmit={action('submitted')} />)
+
+
