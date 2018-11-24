f8f4cd7abed56217da4077367b48b89bc8554890
sebxian <sebxian@gmx.de>
2018-11-24 11:08:37 +0100

    Cleanup

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { BrowserRouter as Router, Route, NavLink as Link } from 'react-router-dom'
-import uid from 'uid';
 
 import TextButton from './TextButton'
 
