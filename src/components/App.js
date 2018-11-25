51471a9fb69d850d82fcbf53d15c68851f19e747
sebxian <sebxian@gmx.de>
2018-11-25 08:49:49 +0100

    Add saving to json

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import { PageWrapper } from './PageWrapper'
 import List from './List.js'
 import Edit from './Edit.js'
 
-let notes = [
+let dummyNotes = [
   { id: uid(), body: 'This is a placeholder note. \n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacus nunc, auctor vestibulum dignissim nec, consectetur ut tellus. In auctor venenatis luctus.' },
   { id: uid(), body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.' },
   { id: uid(), body: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. ' },
@@ -20,7 +20,12 @@ let notes = [
 
 class App extends Component {
 
+  state = {
+    notes: this.load()
+  }
+
   saveNote = (id, body) => {
+    let {notes} = this.state
     if (id !== null) {
       const index = notes.findIndex(item => item.id === id)
       notes[index].body = body
@@ -33,12 +38,12 @@ class App extends Component {
         ...notes
       ]
     }
+    this.setState({ notes: notes })
   }
 
   getExcerpts = () => {
     console.log('get excerpts')
-    console.log(notes)
-    return notes.map(item => {
+    return this.state.notes.map(item => {
       return {
         id: item.id,  
         excerpt: item.body.replace(/(([^\s]+\s\s*){14})(.*)/s, "$1…")
@@ -47,11 +52,24 @@ class App extends Component {
   }
 
   getNoteById = (id) => {
-    const targetId = notes.findIndex(item => item.id === id)
-    return notes[targetId]
+    const targetId = this.state.notes.findIndex(item => item.id === id)
+    return this.state.notes[targetId]
+  }
+
+  save () {
+    localStorage.setItem('Notestack', JSON.stringify(this.state.notes))
+  }
+
+  load () {
+    try {
+      return JSON.parse(localStorage.getItem('Notestack')) || dummyNotes
+    } catch (err) {
+      return dummyNotes
+    }
   }
 
   render () {
+    this.save()
     return (      
       <Router>
         <PageWrapper>
