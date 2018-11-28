cd2b2edfa54d89bb385436862bda13d13bff29c1
Sebastian <sebxian@gmx.de>
2018-11-28 11:38:18 +0100

    Improved Card styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,12 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const CardWrapper = styled.section`
+  margin: 12px 0;
+  border: 1px solid #ccc;
+  padding: 16px 8px;
+  &:hover {
+    border-color: deeppink;
+  }
   &>a {
   color: inherit;
   text-decoration: none;
@@ -10,12 +16,9 @@ const CardWrapper = styled.section`
 `
 
 const CardContent = styled.div`
-  margin: 12px 0;
-  border: 1px solid #ccc;
-  padding: 16px 8px;
-  &:hover {
-    border-color: deeppink;
-  }
+  max-height: 104px;
+  overflow: hidden;
+  line-height: 1.5;
 `
 
 export default class Card extends Component {
