10fdedd00d77c66be6b2d329b18c937948400f1c
Sebastian <sebxian@gmx.de>
2018-11-28 10:54:33 +0100

    Improve NewNoteFooter styling

--- src/components/NewNoteFooter.js
+++ src/components/NewNoteFooter.js
@@ -4,42 +4,36 @@ import { Link } from 'react-router-dom'
 import Icon from './Icon'
 
 const Footer = styled.footer`
+  & a:hover {
+      cursor: text;
+    }
+`
+
+const FooterContent = styled.div`
   margin: 0 5px;
   padding: 5px 2px;
   border-top: 1px solid black;
   display: flex;
   justify-content: flex-start;
   align-items: center;
-
-  & a {
-    display: inherit;
-    & :hover {
-      cursor: text;
-    }
-    & * {
-      align-self: center;
-    }
-  }
 `
 
 const Placeholder = styled.span`
   color: #999;
   width: 100%;
   margin-left: 5px;
-
-  &:hover {
-    color: #999;
-  }
-  `
+`
 
 
 export default class NewNoteFooter extends Component {
   render () {
     return (
       <Footer>
-        <Link to="/create">
-          <Icon name="plus-circle" />
-          <Placeholder>Add a note...</Placeholder>
+        <Link to="/create" >
+          <FooterContent>
+            <Icon name="plus-circle" />
+            <Placeholder>Add a note...</Placeholder>
+          </FooterContent>
         </Link>
       </Footer>
     )
