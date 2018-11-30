464f916515ac2539be9a2475d1ece43489523375
Sebastian <sebxian@gmx.de>
2018-11-30 16:37:09 +0100

    Test branch deployment

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import uid from 'uid';
 import List from './List.js'
 import Edit from './Edit.js'
 
+// foo
+
 let dummyNotes = [
   { id: uid(), body: 'This is a placeholder note. \n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacus nunc, auctor vestibulum dignissim nec, consectetur ut tellus. In auctor venenatis luctus.' },
   { id: uid(), body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.' },
